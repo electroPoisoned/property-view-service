f951387ba5f87f304a51928da7d62f6716e4b02c
Vladislav Daniliuk <vladislavdaniluk192@gmail.com>
2025-08-01 13:27:55 +0300

    Init

---
+++ src/main/java/by/daniliuk/property_view_service/PropertyViewServiceApplication.java
@@ -0,0 +1,13 @@
+package by.daniliuk.property_view_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyViewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyViewServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/daniliuk/property_view_service/PropertyViewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.daniliuk.property_view_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyViewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
